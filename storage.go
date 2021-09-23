9f05a9bb7f77a141da276def4ac139834f7106fa
abyss-w <mad.hatter@foxmail.com>
2021-09-23 15:10:51 +0800

    feat: Add support for Write Empty File Behavior

--- storage.go
+++ storage.go
@@ -578,6 +578,15 @@ func (s *Storage) write(ctx context.Context, path string, r io.Reader, size int6
 		return
 	}
 
+	// According to GSP-751, we should allow the user to pass in a nil io.Reader.
+	// Since oss supports reader passed in as nil, we do not need to determine the case where the reader is nil and the size is 0.
+	// ref: https://github.com/beyondstorage/go-storage/blob/master/docs/rfcs/751-write-empty-file-behavior.md
+	if r == nil && size != 0 {
+		return 0, fmt.Errorf("reader is nil but size is not 0")
+	} else {
+		r = io.LimitReader(r, size)
+	}
+
 	if opt.HasIoCallback {
 		r = iowrap.CallbackReader(r, opt.IoCallback)
 	}
